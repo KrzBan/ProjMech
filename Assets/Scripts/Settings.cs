0de4cb443c6c9b8c82638b05e1d769a61564ae1e
Kamil Butryn <kaybenot@gmail.com>
2024-06-17 22:21:52 +0200

    odpowiedzi

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -41,14 +41,47 @@ public static class Settings
 
     public static List<string> AnswersPositive = new()
     {
-        "haha :D"
+        "<3",
+        "Brzmisz na ciekawą osobę",
+        "Czuję, że nadajemy na tych samych falach",
+        "Słodko <3",
+        "Wow, jesteś naprawdę uroczy!",
+        "Haha, zabawny jesteś, podoba mi się twoje poczucie humoru!",
+        "Dzięki, poprawiłeś mi humor!",
+        "Wydajesz się spoko",
+        "Super, że masz taki luz, naprawdę fajnie się z tobą rozmawia!",
+        "Haha, masz talent do rozbawiania ludzi",
+        "To było naprawdę urocze!",
+        "Doceniam twoje starania!",
+        "Świetny tekst, naprawdę mi się podoba!",
+        "To było kreatywne XD",
+        "Hahaha całkiem niezłe, tak jak Twoje zdjecia",
+        "hahahah <3",
+        "Masz dar do pisania takich tekstów",
+        "To było dobre XD"
     };
-    public static List<string> AnswersNeutral = new()
-    {
-        "Ok"
-    };
+    
     public static List<string> AnswersNegative = new()
     {
-        "ew"
+        "Sorry, ale jestem zajęta",
+        "Emmm...",
+        "Obrzydliwy jesteś",
+        "Powiem szczerze, chyba do siebie nie pasujemy",
+        "Dzięki za wiadomość, ale nie czuję chemii",
+        "Mam wrażenie, że szukamy czegoś innego.",
+        "Chyba nie jestem gotowa na nową znajomość",
+        "Czuję, że szukam kogoś innego typu",
+        "Jesteśmy z dwóch różnych bajek",
+        "Z takimi tekstami to nie dziwię się, że jesteś singlem",
+        "...",
+        "Ty tak na serio? XD",
+        "Fajnie, że próbujesz, ale to nie zadziała.",
+        "Dzięki za wiadomość, ale szukam kogoś, kto bardziej mnie zainteresuje.",
+        "To było słabe...",
+        "To było trochę mniej niż się spodziewałam, a mam serio niskie oczekiwania",
+        "Na razie, to skutecznie mnie zniechęcasz",
+        "Chyba pora usunąć tą aplikacje...",
+        "No i nie podobasz mi się...",
+        "A w domu wszyscy zdrowi?"
     };
 }
